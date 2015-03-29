68ee9a44b3638d5bd7f47a85300c0dbf9db7457f
Javier C <youracow@gmail.com>
2015-03-29 01:04:53 -0600

    added comments for explaination

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,32 +1,53 @@
 (add-to-list 'load-path (expand-file-name "packages" user-emacs-directory))
 (add-to-list 'load-path (expand-file-name "config" user-emacs-directory))
 
-(require 'init-main)
-(require 'init-uxui)
+;; These are main parts of emacs loading
+(require 'init-main) ;; loads 1st
+(require 'init-uxui) ;; loads 2nd
+
+;; You can remove stuff you dont like.
+
+;; SVN
 (require 'init-dsvn)
+;; News reader
 (require 'init-elfeed)
+;; Syntax checkers
 (require 'init-flycheck)
 (require 'init-flycheck-haskell)
 (require 'init-flymake-easy)
 (require 'init-flymake-jslint)
 (require 'init-flymake-ruby)
 (require 'init-haskell-mode)
+;; haskell indent
 (require 'init-hi2)
+;; highlight line
 (require 'init-hlinum)
+;; javascript stuff
 (require 'init-js3-mode)
 (require 'init-json-mode)
 (require 'init-json-reformat)
 (require 'init-json-snatcher)
+;; git integration
 (require 'init-magit)
+;; java
 (require 'init-malabar-mode)
+;; markdown
 (require 'init-markdown-mode)
+;; paren marking
 (require 'init-mic-paren)
+;; html and js support 
 (require 'init-multi-web-mode)
+;; multi cursor like sublime
 (require 'init-multiple-cursors)
+;; file browsers on side
 (require 'init-neotree)
+;; dependency
 (require 'init-prosjekt)
+;; tab with autocomplete
 (require 'init-smart-tab)
+;; undo done better
 (require 'init-undo-tree)
+;; python venv
 (require 'init-virtualenvwrapper)
 
 
