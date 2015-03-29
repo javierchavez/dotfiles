b29307767f9c722cac3d0f63d7b3f2b5e456c52c
Javier C <youracow@gmail.com>
2015-03-28 21:50:41 -0600

    .gitignore is now working

--- emacs.d/elpa/flymake-jslint-20130613.202/flymake-jslint-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "flymake-jslint" "20130613.202" "A flymake handler for javascript using jslint" '((flymake-easy "0.1")) :url "https://github.com/purcell/flymake-jslint")
--- emacs.d/elpa/mic-paren-20140714.719/mic-paren-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "mic-paren" "20140714.719" "advanced highlighting of matching parentheses" 'nil :keywords '("languages" "faces" "parenthesis" "matching"))
