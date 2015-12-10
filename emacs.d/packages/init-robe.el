20565c31ce1259705f63a828d7e53e40c92d0b4e
Javier C <youracow@gmail.com>
2015-12-09 19:47:23 -0700

    new packages for rails dev

---
+++ emacs.d/packages/init-robe.el
@@ -0,0 +1,6 @@
+(require-package 'robe)
+
+;; Package config ;; 
+(add-hook 'ruby-mode-hook 'robe-mode)
+
+(provide 'init-robe)
---
+++ emacs.d/packages/init-yaml-mode.el
@@ -0,0 +1,5 @@
+(require-package 'yaml-mode)
+
+;; Package config ;; 
+
+(provide 'init-yaml-mode)
